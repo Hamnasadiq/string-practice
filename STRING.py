47b75522c995763b0578eb8ca965a596e8fa4893
Hamnasadiq <hmnmhr33@gmail.com>
2024-09-07 20:10:11 +0500

    string

---
+++ STRING.py
@@ -0,0 +1,401 @@
+# lowercase()
+# -----1-----
+name= "HAMna"
+lowercase_name= name.lower()
+print(lowercase_name)
+# -----2-----
+age= "eiGHTeen yearS OLD"
+lowercase_age=age.lower()
+print(lowercase_age)
+# -----3-----
+edu= "inter-ICS PHYsics"
+lowercase_edu=edu.lower()
+print(lowercase_edu)
+# -----4-----
+degree="BS computer SCIEnce"
+lowercase_degree=degree.lower()
+print(lowercase_degree)
+# -----5-----
+piaic="batch-62"
+lowercase_piaic=piaic.lower()
+print(lowercase_piaic)
+# -----6-----
+city="gujRanwaLA"
+lowercase_city=city.lower()
+print(lowercase_city)
+# -----7-----
+hobby=" reading"
+lowercase_hobby=hobby.lower()
+print(lowercase_hobby)
+# -----8-----
+sports="badmiNTOn"
+lowercase_sports=sports.lower()
+print(lowercase_sports)
+# -----9-----
+sweet="chocolate"
+lowercase_sweet=sweet.lower()
+print(lowercase_sweet)
+# -----10-----
+fav_animal="husky"
+lowercase_fav_animal=fav_animal.lower()
+print(lowercase_fav_animal)
+
+#UPPERCASE()
+# -----1-----
+name= "HAMna"
+uppercase_name= name.upper()
+print(uppercase_name)
+# -----2-----
+age= "eiGHTeen yearS OLD"
+uppercase_age=age.upper()
+print(uppercase_age)
+# -----3-----
+edu= "inter-ICS PHYsics"
+uppercase_edu=edu.upper()
+print(uppercase_edu)
+# -----4-----
+degree="BS computer SCIEnce"
+uppercase_degree=degree.upper()
+print(uppercase_degree)
+# -----5-----
+piaic="batch-62"
+uppercase_piaic=piaic.upper()
+print(uppercase_piaic)
+# -----6-----
+city="gujRanwaLA"
+uppercase_city=city.upper()
+print(uppercase_city)
+# -----7-----
+hobby=" reading"
+uppercase_hobby=hobby.upper()
+print(uppercase_hobby)
+# -----8-----
+sports="badmiNTOn"
+uppercase_sports=sports.upper()
+print(uppercase_sports)
+# -----9-----
+sweet="chocolate"
+uppercase_sweet=sweet.upper()
+print(uppercase_sweet)
+# -----10-----
+fav_animal="husky"
+uppercase_fav_animal=fav_animal.upper()
+print(uppercase_fav_animal)
+
+#title
+# -----1-----
+name= "HAMna"
+title_name= name.title()
+print(title_name)
+# -----2-----
+age= "eiGHTeen yearS OLD"
+title_age=age.title()
+print(title_age)
+# -----3-----
+edu= "inter-ICS PHYsics"
+title_edu=edu.title()
+print(title_edu)
+# -----4-----
+degree="BS computer SCIEnce"
+title_degree=degree.title()
+print(title_degree)
+# -----5-----
+piaic="batch-62"
+title_piaic=piaic.title()
+print(title_piaic)
+# -----6-----
+city="gujRanwaLA"
+title_city=city.title()
+print(title_city)
+# -----7-----
+hobby=" reading"
+title_hobby=hobby.title()
+print(title_hobby)
+# -----8-----
+sports="badmiNTOn"
+title_sports=sports.title()
+print(title_sports)
+# -----9-----
+sweet="chocolate"
+title_sweet=sweet.title()
+print(title_sweet)
+# -----10-----
+fav_animal="husky"
+title_fav_animal=fav_animal.title()
+print(title_fav_animal)
+
+#swapcase()
+# -----1-----
+name= "HAMna"
+swap_name= name.swapcase()
+print(swap_name)
+# -----2-----
+age= "eiGHTeen yearS OLD"
+swap_age=age.swapcase()
+print(swap_age)
+# -----3-----
+edu= "inter-ICS PHYsics"
+swap_edu=edu.swapcase()
+print(swap_edu)
+# -----4-----
+degree="BS computer SCIEnce"
+swap_degree=degree.swapcase()
+print(swap_degree)
+# -----5-----
+piaic="batch-62"
+swap_piaic=piaic.swapcase()
+print(swap_piaic)
+# -----6-----
+city="gujRanwaLA"
+swap_city=city.swapcase()
+print(swap_city)
+# -----7-----
+hobby=" reading"
+swap_hobby=hobby.swapcase()
+print(swap_hobby)
+# -----8-----
+sports="badmiNTOn"
+swap_sports=sports.swapcase()
+print(swap_sports)
+# -----9-----
+sweet="chocolate"
+swap_sweet=sweet.swapcase()
+print(swap_sweet)
+# -----10-----
+fav_animal="husky"
+swap_fav_animal=fav_animal.swapcase()
+print(swap_fav_animal)
+
+#capitalize()
+# -----1-----
+name= "HAMna"
+cap_name= name.capitalize()
+print(cap_name)
+# -----2-----
+age= "eiGHTeen yearS OLD"
+cap_age=age.capitalize()
+print(cap_age)
+# -----3-----
+edu= "inter-ICS PHYsics"
+cap_edu=edu.capitalize()
+print(cap_edu)
+# -----4-----
+degree="BS computer SCIEnce"
+cap_degree=degree.capitalize()
+print(cap_degree)
+# -----5-----
+piaic="batch-62"
+cap_piaic=piaic.capitalize()
+print(cap_piaic)
+# -----6-----
+city="gujRanwaLA"
+cap_city=city.capitalize()
+print(cap_city)
+# -----7-----
+hobby=" reading"
+cap_hobby=hobby.capitalize()
+print(cap_hobby)
+# -----8-----
+sports="badmiNTOn"
+cap_sports=sports.capitalize()
+print(cap_sports)
+# -----9-----
+sweet="chocolate"
+cap_sweet=sweet.capitalize()
+print(cap_sweet)
+# -----10-----
+fav_animal="husky"
+cap__fav_animal=fav_animal.capitalize()
+print(cap__fav_animal)
+
+#casefold()
+# -----1-----
+name= "HAMna"
+Casefold__name= name.casefold()
+print(Casefold__name)
+# -----2-----
+german ="der Fluß"
+Casefold_german=german.casefold()
+print(Casefold_german)
+# -----3-----
+edu= "inter-ICS PHYsics"
+Casefold_edu=edu.casefold()
+print(Casefold_edu)
+# -----4-----
+degree="BS computer SCIEnce"
+Casefold_degree=degree.casefold()
+print(Casefold_degree)
+# -----5-----
+piaic="batch-62"
+Casefold_piaic=piaic.casefold()
+print(Casefold_piaic)
+# -----6-----
+city="gujRanwaLA"
+Casefold_city=city.casefold()
+print(Casefold_city)
+# -----7-----
+number="dreißig"
+Casefold_number=number.casefold()
+print(Casefold_number)
+# -----8-----
+color="weiß"
+Casefold_color=color.casefold()
+print(Casefold_color)
+# -----9-----
+sweet="chocolate"
+Casefold_sweet=sweet.casefold()
+print(Casefold_sweet)
+# -----10-----
+fav_animal="husky"
+Casefold_fav_animal=fav_animal.casefold()
+print(Casefold_fav_animal)
+
+#centre()
+# -----1-----
+name= "Hamna"
+centered_name= name.center(10,'*')
+print(centered_name)
+# -----2-----
+age= "eighteen years old"
+centered_age=age.center(50,'-')
+print(centered_age)
+# -----3-----
+edu= "inter- ics physics"
+centered_edu=edu.center(30,'?')
+print(centered_edu)
+# -----4-----
+degree="    BS computer science  "
+centered_degree=degree.center(60,'!')
+print(centered_degree)
+# -----5-----
+piaic="                 batch-62               "
+centered_piaic=piaic.center(100,'@')
+print(centered_piaic)
+# -----6-----
+city="gujranwala"
+centered_city=city.center(500,'#')
+print(centered_city)
+# -----7-----
+hobby=" reading"
+centered_hobby=hobby.center(30,')')
+print(centered_hobby)
+# -----8-----
+sports="badminton"
+centered_sports=sports.center(100,'.')
+print(centered_sports)
+# -----9-----
+sweet="chocolate"
+centered_sweet=sweet.center(50,'"')
+print(centered_sweet)
+# -----10-----
+fav_animal="      husky    "
+centered_fav_animal=fav_animal.center(10,'t')
+print(centered_fav_animal)
+
+#endswith()
+ 
+# -----1-----
+name= "hamna sadiq iqbal."
+print(name.endswith("iqbal."))
+# -----2-----
+age= "eighteen years old."
+print(age.endswith("old"))
+# -----3-----
+edu= "inter-ICS Physics"
+print(edu.endswith("inter"))
+# -----4-----
+degree="BS computer science"
+print(degree.endswith("BS c", 0,4))
+# -----5-----
+piaic="batch-62"
+print(piaic.endswith("tch" ,2,5))
+# -----6-----
+city="gujranwala"
+print(city.endswith("ala" ,7,10))
+# -----7-----
+hobby=" reading"
+print(hobby.endswith("reading"))
+# -----8-----
+sports="badminton"
+print(sports.endswith("nto"))
+# -----9-----
+sweet="chocolate"
+print(sweet.endswith("e"))
+# -----10-----
+fav_animal="husky"
+print(fav_animal.endswith("k"))
+
+#find()
+
+name= "hamna sadiq iqbal"
+index1= name.find("sadiq")
+print(index1)
+# -----2-----
+age= "eighteen years old"
+index2=age.find("old")
+print(index2)
+# -----3-----
+edu= "inter-ICS Physics"
+index3=edu.find( "ysics")
+print(index3)
+# -----4-----
+degree="BS computer Science"
+index4=degree.find("c")
+print(index4)
+# -----5-----
+piaic="batch-62"
+index5=piaic.find("6")
+print(index5)
+# -----6-----
+city="gujranwala"
+index6=city.find("a")
+print(index6)
+# -----7-----
+hobby=" reading"
+index7=hobby.index("ead")
+print(index7)
+# -----8-----
+sports="badminton"
+index8=sports.find("ton")
+print(index8)
+# -----9-----
+sweet="chocolate"
+index9=sweet.find("chocolate")
+print(index9)
+# -----10-----
+fav_animal="husky"
+index10=fav_animal.find("ky")
+print(index10)
+
+#expandtabs()
+
+# -----1-----
+name= "Hamna sadiq \tiqbal"
+print(name.expandtabs())
+# -----2-----
+age= "eighteen\t years old"
+print(age.expandtabs(6))
+# -----3-----
+edu= "inter-ICS\t\t Physics"
+print(edu.expandtabs())
+# -----4-----
+degree="BS computer\t Science"
+print(degree.expandtabs(10))
+# -----5-----
+piaic="batch-62\t"
+print(piaic.expandtabs())
+# -----6-----
+city="gujr\tanwala"
+print(city.expandtabs(0))
+# -----7-----
+hobby=" \treading"
+print(hobby.expandtabs(9))
+# -----8-----
+sports="badminton\t"
+print(sports.expandtabs())
+# -----9-----
+sweet="chocolate\t cake"
+print(sweet.expandtabs())
+# -----10-----
+fav_animal="\t husky "
+print(fav_animal.expandtabs())
